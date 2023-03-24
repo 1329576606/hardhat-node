3ebb820e9a3fca7c2701bd32ddd774d668846c8b
XuZhanXin <zhanxin.xu@kikitrade.com>
2023-03-24 18:07:13 +0800

    chain id

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     hardhat: {
-      // 指定任何要启动的节点配置
-      // 更改数据库目录来持久化数据
-      // 设置数据库目录来保存数据（例如，对于Ubuntu设置db的目录）：
-      db: "./data",
+      chainId:31338
     },
   },
 };
